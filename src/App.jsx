83a2a9b30be06edcc6eccb63009bb0118ef3d3fa
avina25 <avinaalmeida9@gmail.com>
2025-11-15 18:55:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProjectsSidebar from "./components/ProjectsSidebar"
+
+function App() {
+ 
+  return (
+    <main className="h-screen my-8">
+      <ProjectsSidebar/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,10 @@
+export default function ProjectsSidebar() {
+  return (
+    <aside className='w-1/3 px-8 y-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl'>
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+    </aside>
+  );
+}
